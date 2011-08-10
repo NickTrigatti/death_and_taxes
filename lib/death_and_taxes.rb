07d1b02d8dfb33a3f42d389b485c9de1820b174d
Guillaume Malette <gmalette@gmail.com>
2011-08-09 22:03:53 -0400

    Fix the generator

--- lib/death_and_taxes.rb
+++ lib/death_and_taxes.rb
@@ -1,3 +1,6 @@
+require 'active_record'
+require 'active_record/version'
+
 require 'active_support/core_ext'
 
 
--- lib/generators/death_and_taxes/migration/migration_generator.rb
+++ lib/generators/death_and_taxes/migration/migration_generator.rb
@@ -1,8 +1,9 @@
+require 'rails/generators/active_record'
 require 'rails/generators/migration'
 
 # Blindly copied from acts-as-taggable-on
 module DeathAndTaxes
-  class MigationGenerator < Rails::Generators::Base
+  class MigrationGenerator < Rails::Generators::Base
     include Rails::Generators::Migration
     
     desc "Generates migrations for taxations"
@@ -12,7 +13,7 @@ module DeathAndTaxes
     end
     
     def self.source_root
-      File.join(File.dirname(__FILE__), 'templates', (orm.to_s unless orm.class.eql?(String))
+      File.join(File.dirname(__FILE__), 'templates', (orm.to_s unless orm.class.eql?(String)))
     end
     
     def self.orm_has_migration?
@@ -24,7 +25,7 @@ module DeathAndTaxes
     end
     
     def create_migration_file
-      if self.class.orm_has_migration
+      if self.class.orm_has_migration?
         migration_template 'migration.rb', 'db/migrate/death_and_taxes_migration'
       end
     end
