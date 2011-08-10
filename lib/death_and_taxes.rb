a62c678325a2e196c82935869372f9152cfc32fa
Guillaume Malette <gmalette@gmail.com>
2011-08-09 21:50:12 -0400

    Autoload Hooks and instance methods

--- lib/death_and_taxes.rb
+++ lib/death_and_taxes.rb
@@ -1,2 +1,9 @@
 require 'active_support/core_ext'
+
+
 require File.join(File.dirname(__FILE__), 'death_and_taxes/railtie')
+
+module DeathAndTaxes
+  autoload :Hook,            File.join(File.dirname(__FILE__), "death_and_taxes/hook")
+  autoload :InstanceMethods, File.join(File.dirname(__FILE__), "death_and_taxes/instance_methods")  
+end
