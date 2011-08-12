5361bc680229d47bea4ca20d774d1b8ace4e7b63
Guillaume Malette <gmalette@mentel.com>
2011-08-12 15:09:00 -0400

    Add tax validations

--- lib/death_and_taxes/tax.rb
+++ lib/death_and_taxes/tax.rb
@@ -3,12 +3,19 @@ module DeathAndTaxes
     belongs_to :taxer, :polymorphic => true
     belongs_to :tax, :foreign_key => :apply_on_tax
     
+    validates :percentage, :numericality => {:greater_than => 0.01, :less_than => 100}, :format => /^\d+??(?:\.\d*)?$/, :presence => true
+    validates_presence_of :name
+    
     def apply amount
       amount * multiplier
     end
     
     def multiplier
       (percentage / 100) * (1 + tax.try(:multiplier).to_f)
+    end
+    
+    def blank?
+      percentage.blank? && name.blank? && account_number.blank?
     end
   end
 end
